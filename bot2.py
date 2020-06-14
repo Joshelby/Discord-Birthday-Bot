014ef0473a7a5a9c8eb174af06e09df69b427c15
Joshelby <64439962+Joshelby@users.noreply.github.com>
2020-06-14 19:31:26 +0100

    Work up to Sunday evening

---
+++ bot2.py
@@ -0,0 +1,78 @@
+# bot.py
+import os
+
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+import datetime
+import time as t
+import schedule
+import helpers
+
+days_of_months = {"01": "31", "02": "29", "03": "31", "04": "30", "05": "31", "06": "30", "07": "31", "08": "31", "09": "30", "10": "31", "11": "30", "12": "31"}
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_error(self, event, *args, **kwargs):
+        with open('err.log', 'a') as f:
+            if event == 'on_message':
+                f.write(f'Unhandled message: {args[0]}\n')
+            else:
+                raise
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    
+@bot.event
+async def on_guild_join(guild):
+    bdays_file = open(str(guild.id) + "birthdays.txt", mode="w")
+    bdays_file.write("user,bday\n")
+    bdays_file.close()
+    chan_file = open(str(guild.id) + "channel.txt", mode="w")
+    chan = guild.system_channel
+    chan_file.write(str(chan.id))
+    chan_file.close()
+    await guild.system_channel.send("Thank you for using Birthday Bot.\nPlease use the command `!change-channel` to set the default output channel for this bot, otherwise this channel will be the default output channel.\nOnce this is done, use `!add-birthday` to add your birthday to the list!")
+
+@bot.event
+async def on_guild_remove(guild):
+    os.remove(str(guild.id) + "birthdays.txt")
+    os.remove(str(guild.id) + "channel.txt")
+    
+@tasks.loop(hours=1.0, reconnect = False)
+async def check_bdays():
+    if datetime.datetime.now().hour == 19:
+        await helpers.check_bdays(bot)
+
+@check_bdays.before_loop
+async def before_check_bdays():
+    print('waiting...')
+    await bot.wait_until_ready()
+
+@bot.command(name='add-birthday', help='Use this to add your birthday in the format YYYY-MM-DD.')
+async def add_birthday(ctx, bday):
+    if not helpers.validate_bday(bday):
+        await ctx.send("Please enter your birthday in the format YYYY-MM-DD.")
+        return
+    await helpers.add_bday(ctx, bday)
+    
+@bot.command(name='change-birthday', help='Use this to change your currently saved birthday.')
+async def change_birthday(ctx, bday):
+    if not helpers.validate_bday(bday):
+        await ctx.send("Please enter your birthday in the format YYYY-MM-DD.")
+        return
+    await helpers.change_bday(ctx, bday)
+
+@bot.command(name='change-channel', help='Use this to change the output channel for the bot.')
+async def change_channel(ctx, chan):
+    await helpers.change_channel(ctx, chan)
+
+check_bdays.start()
+
+bot.run(TOKEN)
---
+++ helpers.py
@@ -0,0 +1,68 @@
+import datetime
+import csv
+import schedule
+import discord
+
+def validate_bday(bday):
+    try:
+        datetime.date.fromisoformat(bday)
+    except ValueError:
+        return False
+    return True
+
+async def add_bday(ctx, bday):
+    bdays_file = open(str(ctx.guild.id) + "birthdays.txt", mode="r")
+    csv_reader = csv.DictReader(bdays_file, ["user", "bday"])
+    for row in csv_reader:
+        if row["user"] == str(ctx.author.id):
+            print("Wahey")
+            await ctx.send(f"{ctx.author.display_name} already has a birthday stored of {row['bday']}.\nPlease use the change-birthday command if this is incorrect.")
+            return
+    bdays_file.close()
+    bdays_file = open(str(ctx.guild.id) + "birthdays.txt", mode="a")
+    csv_writer = csv.DictWriter(bdays_file, ["user", "bday"])
+    csv_writer.writerow({"user": ctx.author.id, "bday": bday})
+    bdays_file.close()
+    await ctx.send(f"Birthday for {ctx.author.display_name} added successfully!")
+
+async def change_bday(ctx, bday):
+    bdays_file = open(str(ctx.guild.id) + "birthdays.txt", mode="r")
+    csv_reader = csv.DictReader(bdays_file, ["user", "bday"])
+    temp_bdays_dict = {}
+    for row in csv_reader:
+        if row["user"] == str(ctx.author.id):
+            row["bday"] = bday
+        temp_bdays_dict[row["user"]] = row["bday"]
+    print(temp_bdays_dict)
+    bdays_file.close()
+    bdays_file = open(str(ctx.guild.id) + "birthdays.txt", mode="w")
+    csv_writer = csv.DictWriter(bdays_file, ["user", "bday"])
+    for item in temp_bdays_dict.items():
+        csv_writer.writerow({"user": item[0], "bday": item[1]})
+    bdays_file.close()
+    await ctx.send(f"Birthday for {ctx.author.display_name} updated successfully!")
+
+async def change_channel(ctx, chan):
+    channel = discord.utils.get(ctx.guild.text_channels, name=chan)
+    if channel == None:
+        await ctx.channel.send("That channel does not appear to exist, please try again.")
+        return
+    chan_file = open(str(ctx.guild.id) + "channel.txt", mode="w")
+    chan_file.write(str(channel.id))
+    chan_file.close()
+    await ctx.channel.send(f"Output channel successfully changed to {channel.name}.")
+
+async def check_bdays(bot):
+    for guild in bot.guilds:
+        print(guild.name)
+        bdays_file = open(str(guild.id) + "birthdays.txt", mode="r")
+        csv_reader = csv.DictReader(bdays_file, ["user", "bday"])
+        todays_date = datetime.date.today()
+        for row in csv_reader:
+            if row["user"] == "user":
+                continue
+            cur_bday = datetime.date.fromisoformat(row["bday"])
+            if cur_bday.day == todays_date.day and cur_bday.month == todays_date.month:
+                chan_file = open(str(guild.id) + "channel.txt", mode="r")
+                channel = bot.get_channel(int(chan_file.read()))
+                await channel.send(f":partying_face: :birthday:  Happy Birthday <@{row['user']}>!  :birthday: :partying_face:")
